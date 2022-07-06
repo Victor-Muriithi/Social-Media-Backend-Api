bbb3a4496a585a85376fcf2eac7ce7c965a23066
Victor-Muriithi <vmurithi10@gmail.com>
2022-07-06 16:53:08 +0300

    sign up and login

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -24,7 +24,56 @@ const controllers ={
     login: async(req, res)=>{
         const {email, userName, pwd} = req.body
         let pool = await poolPromise();
-        pool.query(`SELECT * FROM `)
-    }
+        pool.query(`SELECT * FROM users WHERE email = '${email}' OR userName = '${userName}' AND userPassword = '${pwd}' `)
+        .then(data =>{
+            if (data.recordset) {
+                return res.status(200).json({
+                    status: 200,
+                    Success: true,
+                    Message: 'Login successful',
+                    Result: data.recordset
+                }) &&
+                    console.log(data.recordset)
+
+            }
+
+            res.status(404).json({
+                Status: 404,
+                Success: false,
+                Message: 'Users not found Please signup',
+                result: []
+            })
+        })
+
+    },
+    signup:async(req, res)=>{
+        const{id, userName, email, pwd} = req.body
+        let pool = await poolPromise()
+        pool.query(`INSERT INTO users (userName, email, userPassword) VALUES('${userName}', '${email}', '${pwd}')`)
+        .then(result=>{
+            if (result.rowsAffected) {
+                return res.status(200).json({
+                    status: 200,
+                    Success: true,
+                    Message: "User Added"
+                }) && console.log("User added")
+            }
+            res.status(403).json({
+                status: 404,
+                Success: false,
+                Message: "User not added"
+            })
+        })
+    },
+
+    addPost:async(res, req)=>{
+        const{id} = req.params
+        const {post, userID} = req.body
+        let pool = await poolPromise()
+        pool.query(`INSERT INTO posts (userID, post) VALUES('${userID}','${post}')`)
+        .then(result=>{
+
+        })
+    },
 }
 module.exports = { ...controllers }
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllPosts,}= require('../controllers/postControllers')
+const {getAllPosts, login, signup,}= require('../controllers/postControllers')
 
 router.get('/posts', getAllPosts)
+router.post('/user/login', login)
+router.post('/user/signup', signup)
 
 
 module.exports = {router}
