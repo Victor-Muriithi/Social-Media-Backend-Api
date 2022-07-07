9d8d4b590c6c9716cd06701ee11059e7c0fef021
Victor-Muriithi <vmurithi10@gmail.com>
2022-07-07 15:58:17 +0300

    addReply

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,105 +1,188 @@
 const poolPromise = require('../config/pool')
 
-const controllers ={
-    getAllPosts: async(req, res)=>{
+const controllers = {
+    getAllPosts: async (req, res) => {
         let pool = await poolPromise();
         pool.request()
-        .execute('dbo.spGetAllPosts')
-        .then(data=>{
-            if (data.recordset) {
-                return res.status(200).json({
-                    Status: 200,
-                    Success: true,
-                    Message: 'posts fetched succesfully',
-                    Result: data.recordset
-                }) && console.log(data.recordset)
-            }
-            res.status(404).json({
-                Status: 404,
-                Success: false,
-                Message: 'post not found',
-                result: []
-            })
-        })
-    },
-
-    login: async(req, res)=>{
-        const {email, pwd} = req.body
-        let pool = await poolPromise();
-        pool.request()
-        .input('email',email)
-        .input('pwd', pwd)
-        .execute('dbo.spLogin')        
-        .then(data =>{
-            if (data.recordset.length == 0) {
+            .execute('dbo.spGetAllPosts')
+            .then(data => {
+                if (data.recordset) {
+                    return res.status(200).json({
+                        Status: 200,
+                        Success: true,
+                        Message: 'posts fetched succesfully',
+                        Result: data.recordset
+                    }) && console.log(data.recordset)
+                }
                 res.status(404).json({
                     Status: 404,
                     Success: false,
-                    Message: 'Users not found Please signup',
+                    Message: 'post not found',
                     result: []
-                });
-                return;
+                })
+            })
+    },
 
-            }
-            res.status(200).json({
-                status: 200,
-                Success: true,
-                Message: 'Login successful',
-                Result: data.recordset
-            }) &&
-                console.log(data.recordset)
+    login: async (req, res) => {
+        const { email, pwd } = req.body
+        let pool = await poolPromise();
+        pool.request()
+            .input('email', email)
+            .input('pwd', pwd)
+            .execute('dbo.spLogin')
+            .then(data => {
+                if (data.recordset.length == 0) {
+                    res.status(404).json({
+                        Status: 404,
+                        Success: false,
+                        Message: 'Users not found Please signup',
+                        result: []
+                    });
+                    return;
 
-        })
+                }
+                res.status(200).json({
+                    status: 200,
+                    Success: true,
+                    Message: 'Login successful',
+                    Result: data.recordset
+                }) &&
+                    console.log(data.recordset)
+
+            })
 
 
     },
-    signup:async(req, res)=>{
-        const{ userName, email, pwd} = req.body
-        let pool = await poolPromise()
-        pool.request()
-        .input('userName', userName)
-        .input('email', email)
-        .input('pwd', pwd)
-        .execute('dbo.spSignUp')
-        .then(result=>{
-            if (result.rowAffected) {
-                return res.status(200).json({
-                    status: 200,
-                    Success: true,
-                    Message: "User Added"
-                }) && console.log("User added")
-            }
+    signup: async (req, res) => {
+        const { userName, email, pwd } = req.body
+        try {
+            let pool = await poolPromise()
+            await pool.request()
+                .input('userName', userName)
+                .input('email', email)
+                .input('pwd', pwd)
+                .execute('dbo.spSignUp')
+                .then(result => {
+                    if (result.rowAffected) {
+                        return res.status(200).json({
+                            status: 200,
+                            Success: true,
+                            Message: "User Added"
+                        }) && console.log("User added")
+                    }
+    
+                })
+            
+        } catch (error) {
+            console.log(error.message)
             res.status(403).json({
                 status: 404,
                 Success: false,
-                Message: "User not added"
-            })
-        })
-    },
-
-    addPost:async(res, req)=>{
-        const {post, userID} = req.body
-        let pool = await poolPromise()
-        pool.request()
-        .input('userId', userID)
-        .input('post', post)
-        .execute('dbo.spAddPost')
-        .then( result =>{
-            if (result.rowsAffected) {
-                return res.status(200).json({
-                    status: 200,
-                    Success: true,
-                    Message: "Post Added Succesfully"
-                }) && console.log("Post Added Succesfully")
-            }   res.status(403).json({
-                status: 404,
-                Success: false,
-                Message: result.recordset
+                Message: error.message
             })
         }
 
-        )
-
     },
+
+    addPost: async (req, res) => {
+        const { userID, post } = req.body;
+        try {
+            let pool = await poolPromise()
+            await pool.request()
+                .input('userID', userID)
+                .input('post', post)
+                .execute('dbo.spAddPosts')
+                .then(result => {
+                    if (result.rowsAffected.length > 0) {
+
+                        res.status(200).json({
+                            status: 200,
+                            Success: false,
+                            Message: "Post added Succesfully"
+                        });
+                        return;
+                    }
+                })
+
+        } catch (error) {
+            res.status(404).json({
+                status: 404,
+                Success: false,
+                Message: error.message
+            }) && console.log(error.message)
+        }
+    },
+
+    addComment:async (req, res)=>{
+       const {userID, postID, comment} = req.body
+       try {
+        let pool = await poolPromise()
+       await pool.request()
+       .input('userID', userID)
+       .input('postID', postID)
+       .input('comment', comment)
+       .execute('dbo.spAddComment')
+       .then(result=>{
+        console.log(result)
+        if(result.rowsAffected > 0){
+            res.status(201).json({
+                status:200,
+                Success:true,
+                Message: 'Comment added successfully'
+            });
+            return;
+        }
+            res.status(403).json({
+                status:403,
+                Success:false,
+                Message:'Failed to add comment'
+            })
+       })
+       } catch (error) {
+        console.log(error.message)
+            res.status(404).json({
+                Status:404,
+                Success:false,
+                Message:error.message
+            })
+       } 
+    },
+
+    addReply:async(req, res)=>{
+        const {commentID, reply}=req.body;
+        try {
+            let pool = await poolPromise()
+            await pool.request()
+            .input('commentID', commentID)
+            .input('reply', reply)
+            .execute('dbo.spAddReply')
+            .then(result=>{
+                console.log(result)
+                if(result.rowsAffected > 0){
+                    res.status(201).json({
+                        status:200,
+                        Success:true,
+                        Message: 'Reply added successfully'
+                    });
+                    return;
+                }
+                    res.status(403).json({
+                        status:403,
+                        Success:false,
+                        Message:'Failed to add reply'
+                    })
+
+            })
+        } catch (error) {
+            console.log(error.message)
+            res.status(404).json({
+                Status:404,
+                Success:false,
+                Message:error.message
+            })
+        }
+    },
+
+
 }
 module.exports = { ...controllers }
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllPosts, login, signup,}= require('../controllers/postControllers')
+const {getAllPosts, login, signup, addPost, addComment, addReply,}= require('../controllers/postControllers')
 
 router.get('/posts', getAllPosts)
 router.post('/user/login', login)
 router.post('/user/signup', signup)
+router.post('/user/addPost', addPost)
+router.post('/post/addComment', addComment)
+router.post('/post/addReply', addReply)
 
 
 module.exports = {router}
